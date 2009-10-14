fb871906e32ce16b4b0b379141a7e360a9356a19
Almad <bugs@almad.net>
2009-10-14 20:33:50 +0200

    Test for proper client->slave propagation

--- tests/unit_project/tests/test_master_config_generation.py
+++ tests/unit_project/tests/test_master_config_generation.py
@@ -6,7 +6,7 @@ from bbmongostatus.status import MongoDb
 from cthulhubot.assignment import Assignment
 from cthulhubot.buildbot import get_buildmaster_config
 from cthulhubot.models import BuildComputer, JobAssignment, Job, Command
-from cthulhubot.views import create_project
+from cthulhubot.views import create_project, create_job_assignment
 
 from unit_project.tests.helpers import MockJob
 from mock import Mock
@@ -18,7 +18,7 @@ class TestSchedulers(DatabaseTestCase):
         self.project = create_project(name=self.project_name, tracker_uri="http://example.com")
         self.buildmaster = self.project.buildmaster_set.all()[0]
 
-        self.computer = BuildComputer.objects.create(hostname="localhost")
+        self.computer = BuildComputer.objects.create(name="localhost")
 
         job = Job.objects.create(slug='cthulhubot-debian-package-creation')
         job.auto_discovery()
@@ -73,13 +73,34 @@ class TestBuildmasterFrontend(DatabaseTestCase):
         self.project_name = u"project"
         self.project = create_project(name=self.project_name, tracker_uri="http://example.com")
         self.buildmaster = self.project.buildmaster_set.all()[0]
+        self.computer_model = BuildComputer.objects.create(name="localhost", hostname="localhost")
+        self.computer = self.computer_model.get_domain_object()
+        self.job = job = Job.objects.create(slug='cthulhubot-sleep')
+        self.job.auto_discovery()
+        self.assignment_model = create_job_assignment(
+            computer = self.computer_model,
+            job = self.job,
+            project = self.project,
+        )
+        self.config = self.buildmaster.get_config()
 
     def test_frontend_is_not_screwing_up(self):
-        model_config = self.buildmaster.get_config()
         master_config = get_buildmaster_config(self.project_name)
 
         for attr in ['projectURL', 'buildbotURL', 'slavePortnum', 'projectName']:
-            self.assert_equals(model_config[attr], master_config[attr])
+            self.assert_equals(self.config[attr], master_config[attr])
+
+    def test_slave_retrieved(self):
+        self.assert_equals(1, len(self.config['slaves']))
+
+    def test_unique_slaves_retrieved(self):
+        # duplicate project@computer
+        create_job_assignment(computer = self.computer_model, job = self.job, project = self.project)
+
+        new_computer = BuildComputer.objects.create(name="blah", hostname="blah")
+        create_job_assignment(computer = new_computer, job = self.job, project = self.project)
+
+        self.assert_equals(2, len(self.config['slaves']))
 
 
     def tearDown(self):
